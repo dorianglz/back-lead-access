13aaffd430d4e26acd3d18476ae8e3163d3cde17
Dorian Gonzalez <dardu@macbook-pro-de-dorian.home>
2024-01-21 20:35:02 +0100

    update

--- app.js
+++ app.js
@@ -33,6 +33,18 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Mettez ici votre domaine autorisé
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    
+    // Si la requête est une OPTIONS, renvoyez une réponse immédiate avec un statut 200
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+});  
 
 // REQUEST
 app.get("/leads", async (req, res) => {
